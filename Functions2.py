67242c11d39df8e631b2b7482890d745c70c9959
KeylessCreature <92558537+KeylessCreature@users.noreply.github.com>
2021-10-19 09:43:11 -0500

    Update Functions2.py

--- Functions2.py
+++ Functions2.py
@@ -1,3 +1,16 @@
 def reverse(word):
     wordlist = []
-    wordlist.append(word[0:1])
+    a = 1
+    b = 0
+    for i in range(len(word)):
+        wordlist.append(word[b:a])
+        a += 1
+        b += 1
+    print(wordlist)
+    c = len(word) - 1
+    word2 = ""
+    for i in range(len(word)):
+        word2 += wordlist[c]
+        c -= 1
+    print(word2)
+reverse("Hello")
